20f10634e4144b2f91a9cb907fd902f893e07989
Jan Krupa <JanKrupa93@gmail.com>
2025-02-28 08:14:06 +0100

    Create basic setup.py file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ipamtree',
+    version='0.1',
+    description='NetBox plugin for having an IPAM tree display',
+    url='https://github.com/Kani999/netbox-ipamtree',
+    author='Your Name',
+    author_email='your.email@example.com',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
